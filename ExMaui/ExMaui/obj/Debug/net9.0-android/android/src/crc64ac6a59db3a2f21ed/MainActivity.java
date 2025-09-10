bb996098bde6af4b8e7ab4628c57a0f8aa5ddb0e
Erick <erick.fares@agriculturaverion.com.br>
2025-09-10 15:17:37 -0300

    Criando pastas

---
+++ ExMaui/ExMaui/obj/Debug/net9.0-android/android/src/crc64ac6a59db3a2f21ed/MainActivity.java
@@ -0,0 +1,46 @@
+package crc64ac6a59db3a2f21ed;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("ExMaui.MainActivity, ExMaui", MainActivity.class, __md_methods);
+	}
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("ExMaui.MainActivity, ExMaui", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("ExMaui.MainActivity, ExMaui", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
